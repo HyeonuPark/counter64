61e0c0d6ccdc84171a9d89fae88adcfd75ec1c56
Hyeonu Park <nemo1275@gmail.com>
2018-04-13 13:01:42 +0900

    make test overflows u32::MAX

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,10 @@ mod counter {
             COUNTER_INIT
         }
 
+        pub unsafe fn with_init(num: u64) -> Self {
+            Counter(AtomicUsize::new(num as usize))
+        }
+
         /// Get counter's current value
         pub fn get(&self) -> u64 {
             self.0.load(Relaxed) as u64
@@ -56,6 +60,13 @@ mod counter {
         /// Create new counter from 0
         pub fn new() -> Self {
             COUNTER_INIT
+        }
+
+        pub unsafe fn with_init(num: u64) -> Self {
+            Counter {
+                n1: AtomicUsize::new((num & 0xFFFFFFFF) as usize),
+                n2: AtomicUsize::new((num >> 32) as usize),
+            }
         }
 
         /// Get counter's current value
@@ -112,6 +123,15 @@ mod counter {
             COUNTER_INIT
         }
 
+        pub unsafe fn with_init(num: u64) -> Self {
+            Counter {
+                n1: AtomicUsize::new((num & 0xFFFF) as usize),
+                n2: AtomicUsize::new(((num >> 16) & 0xFFFF) as usize),
+                n3: AtomicUsize::new(((num >> 32) & 0xFFFF) as usize),
+                n4: AtomicUsize::new(((num >> 48) & 0xFFFF) as usize),
+            }
+        }
+
         /// Get counter's current value
         pub fn get(&self) -> u64 {
             loop {
@@ -148,93 +168,6 @@ mod counter {
     }
 }
 
-// Probably not even needed, but why not?
-#[cfg(target_pointer_width = "8")]
-mod counter {
-    use std::sync::atomic::{AtomicUsize, ATOMIC_USIZE_INIT, Ordering as O};
-    use std::usize;
-
-    #[derive(Debug)]
-    pub struct Counter {
-        n1: AtomicUsize,
-        n2: AtomicUsize,
-        n3: AtomicUsize,
-        n4: AtomicUsize,
-        n5: AtomicUsize,
-        n6: AtomicUsize,
-        n7: AtomicUsize,
-        n8: AtomicUsize,
-    }
-
-    pub const COUNTER_INIT: Counter = Counter {
-        n1: ATOMIC_USIZE_INIT,
-        n2: ATOMIC_USIZE_INIT,
-        n3: ATOMIC_USIZE_INIT,
-        n4: ATOMIC_USIZE_INIT,
-        n5: ATOMIC_USIZE_INIT,
-        n6: ATOMIC_USIZE_INIT,
-        n7: ATOMIC_USIZE_INIT,
-        n8: ATOMIC_USIZE_INIT,
-    };
-
-    impl Counter {
-        /// Create new counter from 0
-        pub fn new() -> Self {
-            COUNTER_INIT
-        }
-
-        /// Get counter's current value
-        pub fn get(&self) -> u64 {
-            loop {
-                let n1 = self.n1.load(O::SeqCst);
-                let n2 = self.n2.load(O::SeqCst);
-                let n3 = self.n3.load(O::SeqCst);
-                let n4 = self.n4.load(O::SeqCst);
-                let n5 = self.n1.load(O::SeqCst);
-                let n6 = self.n2.load(O::SeqCst);
-                let n7 = self.n3.load(O::SeqCst);
-                let n8 = self.n4.load(O::SeqCst);
-
-                if n1 == self.n1.load(O::SeqCst) {
-                    let mut count = 0u64;
-                    count += n8;
-                    count <<= 8;
-                    count += n7;
-                    count <<= 8;
-                    count += n6;
-                    count <<= 8;
-                    count += n5;
-                    count <<= 8;
-                    count += n4;
-                    count <<= 8;
-                    count += n3;
-                    count <<= 8;
-                    count += n2;
-                    count <<= 8;
-                    count += n1;
-
-                    return count;
-                }
-            }
-        }
-
-        /// Increase counter by 1, and return previous value
-        pub fn incr(&self) -> u64 {
-            let _ =
-                self.n1.fetch_add(1, O::Release) == usize::MAX &&
-                self.n2.fetch_add(1, O::Release) == usize::MAX &&
-                self.n3.fetch_add(1, O::Release) == usize::MAX &&
-                self.n4.fetch_add(1, O::Release) == usize::MAX &&
-                self.n5.fetch_add(1, O::Release) == usize::MAX &&
-                self.n6.fetch_add(1, O::Release) == usize::MAX &&
-                self.n7.fetch_add(1, O::Release) == usize::MAX &&
-                self.n8.fetch_add(1, O::Release) == usize::MAX;
-
-            self.get()
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -243,7 +176,10 @@ mod tests {
 
     #[test]
     fn test_multithread_incr() {
-        let counter = Arc::new(Counter::new());
+        use std::u32;
+
+        let counter = unsafe { Counter::with_init(u32::MAX as u64 - 80000) };
+        let counter = Arc::new(counter);
 
         let handles: Vec<_> = (0..8)
             .map(|_| {
@@ -260,7 +196,7 @@ mod tests {
             handle.join().unwrap();
         }
 
-        assert_eq!(counter.get(), 640000);
+        assert_eq!(counter.get(), u32::MAX as u64 - 80000 + 640000);
 
     }
 }
